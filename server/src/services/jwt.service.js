42cc5cb63e899033cb27f5c2f9fca04d5884cc95
madhusailesh <madhusaileshsasamal998@gmail.com>
2026-07-19 21:53:28 +0530

    feat(auth): add verifyRefreshToken function to validate refresh tokens

--- server/src/services/jwt.service.js
+++ server/src/services/jwt.service.js
@@ -25,8 +25,11 @@ const generateRefreshToken = (user) => {
     }
   );
 };
-
+const verifyRefreshToken = (token) => {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+};
 module.exports = {
   generateAccessToken,
   generateRefreshToken,
+  verifyRefreshToken,
 };
