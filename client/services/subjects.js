65202cbb0d0acff4eb853839722356e0567049ee
madhusailesh <madhusaileshsasamal998@gmail.com>
2026-07-22 01:30:10 +0530

    feat: add getSubjectByCode function to fetch subject details by code

--- client/services/subjects.js
+++ client/services/subjects.js
@@ -9,3 +9,7 @@ export const getSubjectsBySemester = async (semester) => {
   const { data } = await api.get(`/subjects?semester=${semester}`);
   return data.data;
 };
+export const getSubjectByCode = async (code) => {
+  const { data } = await api.get(`/subjects/code/${code}`);
+  return data.data;
+};
